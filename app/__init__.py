0dcdf5ba821ba6a5629b185f878d5856c504af9a
Danny Pernik <danny@opencurrents.com>
2020-11-05 17:31:08 -0600

    rotatingfilehandler

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_bootstrap import Bootstrap
 from flask_mailman import Mail
 
