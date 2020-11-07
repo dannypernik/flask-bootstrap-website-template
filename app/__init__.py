5ec2e575772da754030c30e06330aaea85cd887f
Danny Pernik <danny@opencurrents.com>
2020-11-06 23:14:35 -0600

    sentry debug

--- app/__init__.py
+++ app/__init__.py
@@ -38,7 +38,7 @@ if not app.debug:
 
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+        file_handler = RotatingFileHandler('logs/tutoring.log', maxBytes=10240,
                                            backupCount=10)
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
