1547b2de9a506dc7d96d2534230081b9dc0f1a54
Danny Pernik <danny@opencurrents.com>
2020-11-06 23:38:25 -0600

    debug

--- tutoring.py
+++ tutoring.py
@@ -1,5 +1,13 @@
 from app import app, db
 from app.models import User, Post
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    "https://7b07a0f6a2244061a029e1deaa863161@o473156.ingest.sentry.io/5507799",
+    traces_sample_rate=1.0, integrations=[FlaskIntegration()])
+
+division_by_zero = 1 / 0
 
 @app.shell_context_processor
 def make_shell_context():
