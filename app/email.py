d5b3908584c76b80d429f44d59725485e9c49426
Danny Pernik <danny@opencurrents.com>
2020-12-25 01:48:21 -0600

    fix reply_to, update admin address

--- app/email.py
+++ app/email.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from flask_mailman import Message
+from flask_mailman import EmailMessage
 from app import mail, app
 from flask import render_template
 
@@ -7,18 +7,14 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-def send_email(subject, sender, recipients, reply_to, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients.split())
-    msg.body = text_body
-    msg.html = html_body
+def send_email(subject, text_body, from_email, to, reply_to):
+    msg = EmailMessage(subject=subject, body=text_body, from_email=from_email, to=to.split(), reply_to=reply_to)
     Thread(target=send_async_email, args=(app, msg)).start()
 
 def send_inquiry_email(user, subject, message):
     send_email(subject,
-               sender=("Ascended Learning", "ascendedlearningtutoring@gmail.com"),
-               recipients=app.config['ADMINS'][0],
-               reply_to=user.email,
+               from_email=("OpenPath Tutoring", app.config['MAIL_USERNAME']),
+               to=app.config['ADMINS'][0],
+               reply_to=[user.email],
                text_body=render_template('email/inquiry-form.txt',
-                                        user=user, message=message),
-               html_body=render_template('email/inquiry-form.html',
                                         user=user, message=message))
