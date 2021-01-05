dd045b69b1e233e63725860fec8213619c8ada80
Danny Pernik <dannypernik@gmail.com>
2021-01-05 11:53:25 -0600

    bootstrap 4 attempt, about page

--- app/static/js/smooth-scroll.js
+++ app/static/js/smooth-scroll.js
@@ -1,8 +1,8 @@
-const link = document.querySelectorAll("a");
+const link = document.querySelectorAll(".home-next");
 link.forEach(elem => elem.addEventListener("click", smoothscroll));
 function smoothscroll(event){
   event.preventDefault();
-  const targetId = event.currentTarget.getAttribute("href");
+  const targetId = event.currentTarget.getElementsByTagName('a')[0].getAttribute("href");
   window.scrollTo({
     top: document.querySelector(targetId).offsetTop,
     behavior: "smooth"
