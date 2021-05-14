1885fb56ead8c0b91b94f679bd406eb9471c5f21
dannypernik <dannypernik@mail.com>
2021-05-14 00:06:58 -0500

    add timestamp to updated css versions; code org

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, flash, Markup, redirect, url_for, request, send_from_directory
 from app import app, db
 from app.forms import InquiryForm, EmailForm, SignupForm, LoginForm, EditProfileForm
@@ -13,6 +14,11 @@ def before_request():
         current_user.last_viewed = datetime.utcnow()
         db.session.commit()
 
+def dir_last_updated(folder):
+    return str(max(os.path.getmtime(os.path.join(root_path, f))
+                   for root_path, dirs, files in os.walk(folder)
+                   for f in files))
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -26,7 +32,7 @@ def index():
         print(app.config['ADMINS'])
         flash("Thank you for your message. We will be in touch!")
         return redirect(url_for('index'))
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, last_updated=dir_last_updated('app/static'))
 
 @app.route('/about')
 def about():
