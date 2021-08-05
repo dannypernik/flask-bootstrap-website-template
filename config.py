3f5d39536a93d1eb03aa74850c26e049115aa12a
dannypernik <dannypernik@mail.com>
2021-08-05 11:10:47 -0500

    add admins to config

--- config.py
+++ config.py
@@ -16,4 +16,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['danny@openpathtutoring.com']
+    ADMINS = [os.environ.get('ADMINS')]
