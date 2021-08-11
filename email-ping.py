c0f9ca5e096e77f2e28cc3243293704b19d085e8
dannypernik <dannypernik@mail.com>
2021-08-11 14:11:49 -0500

    email test

--- email-ping.py
+++ email-ping.py
@@ -2,7 +2,7 @@
 import smtplib
 import time
 from email.mime.text import MIMEText
-from app import app.config
+from app import config
 
 to_email = app.config['MAIL_USERNAME']  # add recipient (your remote account) here
 from_email = app.config['MAIL_USERNAME']  # email from which the e-mail is sent; must be accepted by SMTP
