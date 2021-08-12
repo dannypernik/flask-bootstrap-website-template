755f59d8297ef85f0da83f0e3e8fe7a633375c96
dannypernik <dannypernik@mail.com>
2021-08-11 20:03:14 -0500

    credentials loc

--- reminders.py
+++ reminders.py
@@ -36,7 +36,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                os.path.join(basedir, 'credentials.json'), SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
