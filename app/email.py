494ac6f52cedf734535028720701b6d871dc1786
dannypernik <dannypernik@mail.com>
2021-08-29 00:21:14 -0500

    update emails

--- app/email.py
+++ app/email.py
@@ -2,10 +2,9 @@ from threading import Thread
 from app import app
 from mailjet_rest import Client
 from flask import render_template
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+import re
+import datetime
+from dateutil.parser import parse
 
 def send_contact_email(user, message):
     api_key = app.config['MAILJET_KEY']
@@ -31,3 +30,119 @@ def send_contact_email(user, message):
             }
         ]
     }
+
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
+
+
+def send_reminder_email(event, student, quote):
+    api_key = app.config['MAILJET_KEY']
+    api_secret = app.config['MAILJET_SECRET']
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+
+    dt = datetime.datetime
+
+    start_date = dt.strftime(parse(event['start'].get('dateTime')), format="%A, %b %-d, %Y")
+    start_time = event['start'].get('dateTime')
+    start_time_formatted = re.sub(r'([-+]\d{2}):(\d{2})(?:(\d{2}))?$', r'\1\2\3', start_time)
+    start_offset = dt.strptime(start_time_formatted, "%Y-%m-%dT%H:%M:%S%z") + datetime.timedelta(hours = student.timezone)
+    end_time = event['end'].get('dateTime')
+    end_time_formatted = re.sub(r'([-+]\d{2}):(\d{2})(?:(\d{2}))?$', r'\1\2\3', end_time)
+    end_offset = dt.strptime(end_time_formatted, "%Y-%m-%dT%H:%M:%S%z") + datetime.timedelta(hours = student.timezone)
+    start_display = dt.strftime(start_offset, "%-I:%M") + dt.strftime(start_offset, "%p").lower()
+    end_display = dt.strftime(end_offset, "%-I:%M") + dt.strftime(end_offset, "%p").lower()
+    if quote is not None:
+        message = quote.json()['contents']['quotes'][0]['quote']
+        author = quote.json()['contents']['quotes'][0]['author']
+        quote_header = "<strong>Random inspirational quote of the day:</strong><br/>"
+    else:
+        quote_header = ""
+        message = "We don't have to do all of it alone. We were never meant to."
+        author = "Brene Brown"
+
+    if student.timezone is -2:
+        timezone = "Pacific"
+    elif student.timezone is -1:
+        timezone = "Mountain"
+    elif student.timezone is 0:
+        timezone = "Central"
+    elif student.timezone is 1:
+        timezone = "Eastern"
+    else:
+        timezone = "your"
+
+    data = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": app.config['MAIL_USERNAME'],
+                    "Name": "Danny Pernik"
+                },
+                "To": [
+                    {
+                    "Email": student.student_email
+                    },
+                    {
+                    "Email": student.parent_email
+                    }
+                ],
+                "Bcc": [
+                    {
+                    "Email": app.config['MAIL_USERNAME']
+                    }
+                ],
+                "Subject": "Reminder for " + event.get('summary') + " + a quote from " + author,
+                "HTMLPart": "Hello, this is an automated reminder that " + student.student_name + \
+                    " is scheduled for a tutoring session on " + start_date + " from  " + \
+                    start_display + " to " + end_display + " " + timezone + " time. <br/><br/>" + \
+                    "Location: " + student.location + "<br/><br/>" + \
+                    "You are welcome to reply to this email with any questions. " + \
+                    "Please provide at least 24 hours notice when cancelling or rescheduling " + \
+                    "in order to avoid losing the session. Note that you will not receive a " + \
+                    "reminder email for sessions scheduled with less than 2 days notice.<br/><br/>" + \
+                    "Thank you,<br/>Danny <br/><br/><br/>" + \
+                    quote_header + '"' + message + '"' + "<br/>&mdash; " + author
+            }
+        ]
+    }
+
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
+
+
+def weekly_report_email(sessions, hours, students, now):
+    api_key = app.config['MAILJET_KEY']
+    api_secret = app.config['MAILJET_SECRET']
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+
+    dt = datetime.datetime
+    start = (now + datetime.timedelta(hours=39)).isoformat() + 'Z'
+    start_date = dt.strftime(parse(start), format="%A, %b %-d @ %-I:%M%p")
+    end = (now + datetime.timedelta(days=7, hours=31)).isoformat() + 'Z'
+    end_date = dt.strftime(parse(end), format="%A, %b %-d @ %-I:%M%p")
+
+    data = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": app.config['MAIL_USERNAME'],
+                    "Name": "Danny Pernik"
+                },
+                "To": [
+                    {
+                    "Email": app.config['MAIL_USERNAME']
+                    }
+                ],
+                "Subject": "Scheduled tutoring from " + start_date + " to " + end_date,
+                "HTMLPart": "Scheduled sessions: " + sessions + "<br/>" + \
+                    "Scheduled hours: " + hours + \
+                    "<br/>Active students: " + students
+            }
+        ]
+    }
+
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
--- reminders.py
+++ reminders.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import datetime
-from dateutil.parser import parse
+from dateutil.parser import parse, isoparse
 from dateutil import tz
 import os.path
 from googleapiclient.discovery import build
@@ -8,10 +8,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow, Flow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from app import app
-from mailjet_rest import Client
 from dotenv import load_dotenv
 from app.models import Student
-import re
+from app.email import send_reminder_email, weekly_report_email
 import requests
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -67,8 +66,9 @@ def main():
     students = Student.query.order_by(-Student.id).all()
     reminder_list = []
     quote = requests.get("https://quotes.rest/qod?category=inspire&language=en")
-    print(quote.text)
-    #author = response.json(author())
+
+    if quote.status_code is not 200:
+        quote = None
 
     for event in events:
         for student in students:
@@ -84,75 +84,28 @@ def main():
         print("No reminders sent.")
 
 
-def send_reminder_email(event, student, quote):
-    api_key = app.config['MAILJET_KEY']
-    api_secret = app.config['MAILJET_SECRET']
-    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-
-    dt = datetime.datetime
-
-    start_date = dt.strftime(parse(event['start'].get('dateTime')), format="%A, %b %-d %Y")
-    start_time = event['start'].get('dateTime')
-    start_time_formatted = re.sub(r'([-+]\d{2}):(\d{2})(?:(\d{2}))?$', r'\1\2\3', start_time)
-    start_offset = dt.strptime(start_time_formatted, "%Y-%m-%dT%H:%M:%S%z") + datetime.timedelta(hours = student.timezone)
-    end_time = event['end'].get('dateTime')
-    end_time_formatted = re.sub(r'([-+]\d{2}):(\d{2})(?:(\d{2}))?$', r'\1\2\3', end_time)
-    end_offset = dt.strptime(end_time_formatted, "%Y-%m-%dT%H:%M:%S%z") + datetime.timedelta(hours = student.timezone)
-    start_display = dt.strftime(start_offset, "%-I:%M") + dt.strftime(start_offset, "%p").lower()
-    end_display = dt.strftime(end_offset, "%-I:%M") + dt.strftime(end_offset, "%p").lower()
-    message = quote.json()['contents']['quotes'][0]['quote']
-    author = quote.json()['contents']['quotes'][0]['author']
-
-    if student.timezone is -2:
-        timezone = "Pacific"
-    elif student.timezone is -1:
-        timezone = "Mountain"
-    elif student.timezone is 0:
-        timezone = "Central"
-    elif student.timezone is 1:
-        timezone = "Eastern"
-    else:
-        timezone = "your"
-
-    data = {
-        'Messages': [
-            {
-            "From": {
-                "Email": app.config['MAIL_USERNAME'],
-                "Name": "Danny Pernik"
-            },
-            "To": [
-                {
-                "Email": student.student_email
-                },
-                {
-                "Email": student.parent_email
-                }
-            ],
-            "Bcc": [
-                {
-                "Email": app.config['MAIL_USERNAME']
-                }
-            ],
-            "Subject": "Reminder for " + event.get('summary') + " + a quote from " + author,
-            "HTMLPart": "Hello, this is an automated reminder that " + student.student_name + \
-            " is scheduled for a tutoring session on " + start_date + " from  " + \
-            start_display + " to " + end_display + " " + timezone + " time. <br/><br/>" + \
-            "Location: " + student.location + "<br/><br/>" + \
-            "You are welcome to reply to this email with any questions. " + \
-            "Please provide at least 24 hours notice when cancelling or rescheduling " + \
-            "in order to avoid losing the session. Note that you will not receive a " + \
-            "reminder email for sessions scheduled with less than 2 days notice.<br/><br/>" + \
-            "Thank you,<br/>Danny <br/><br/><br/>" + \
-            "<strong>Random inspirational quote of the day:</strong><br/>"
-            '"' + message + '"' + "<br/>&mdash; " + author
-            }
-        ]
-    }
-
-    result = mailjet.send.create(data=data)
-    print(result.status_code)
-    print(result.json())
+    day_of_week = datetime.datetime.strftime(parse(now), format="%A")
+    week_end = (today + datetime.timedelta(days=7, hours=31)).isoformat() + 'Z'
+    week_events_result = service.events().list(calendarId='primary', timeMin=upcoming_start,
+                                        timeMax=week_end, singleEvents=True,
+                                        orderBy='startTime').execute()
+    week_events = week_events_result.get('items', [])
+    session_count = 0
+    tutoring_hours = 0
+    print(day_of_week)
+    if day_of_week == "Friday":
+        for e in week_events:
+            for s in students:
+                if " " + s.student_name + " " in e.get('summary'):
+                    session_count += 1
+                    start = isoparse(e['start'].get('dateTime'))
+                    end = isoparse(e['end'].get('dateTime'))
+                    duration = str(end - start)
+                    (h, m, s) = duration.split(':')
+                    hours = int(h) + int(m) / 60 + int(s) / 3600
+                    tutoring_hours += hours
+        print(upcoming_start, week_end)
+        weekly_report_email(str(session_count), str(tutoring_hours), str(len(students)), today)
 
 
 if __name__ == '__main__':
