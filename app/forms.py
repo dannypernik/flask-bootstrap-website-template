b37443df65476aa4c1f699f75a11a3f9554f292c
dannypernik <dannypernik@mail.com>
2022-08-31 00:30:25 -0500

    make late_date optional

--- app/forms.py
+++ app/forms.py
@@ -117,7 +117,7 @@ class TestDateForm(FlaskForm):
         validators=[InputRequired()])
     date = DateField('Test date', format='%Y-%m-%d', validators=[InputRequired()])
     reg_date = DateField('Registration deadline', format='%Y-%m-%d')
-    late_date = DateField('Late deadline', format='%Y-%m-%d')
+    late_date = DateField('Late deadline', format='%Y-%m-%d', validators=(validators.Optional(),))
     other_date = DateField('Other deadline', format='%Y-%m-%d', validators=(validators.Optional(),))
     status = SelectField('Status', choices=[('confirmed','Confirmed'),('unconfirmed','Unconfirmed'),('past','Past')])
     submit = SubmitField('Save')
