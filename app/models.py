2e404cab4238da304ec6794859fbccdb4b42146a
dannypernik <dannypernik@mail.com>
2022-09-26 00:51:28 -0500

    sat-act-data ready

--- app/models.py
+++ app/models.py
@@ -10,12 +10,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(32), index=True)
     last_name = db.Column(db.String(32), index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), index=True)
     phone = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(128))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    about_me = db.Column(db.String(500))
     last_viewed = db.Column(db.DateTime, default=datetime.utcnow)
     is_admin = db.Column(db.Boolean)
 
