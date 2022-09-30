b9531cbcb030a9b885c2376be3ca3c1fd487e2b8
dannypernik <dannypernik@mail.com>
2022-09-29 23:35:04 -0500

    remove unneeded form; styling

--- app/__init__.py
+++ app/__init__.py
@@ -12,15 +12,6 @@ from flask_bootstrap import Bootstrap
 from flask_hcaptcha import hCaptcha
 from functools import wraps
 
-# convention = {
-#     "ix": 'ix_%(column_0_label)s',
-#     "uq": "uq_%(table_name)s_%(column_0_name)s",
-#     "ck": "ck_%(table_name)s_%(constraint_name)s",
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-#     "pk": "pk_%(table_name)s"
-# }
-
-# metadata = MetaData(naming_convention=convention)
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
