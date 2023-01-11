d25165f044889fae2766e2d57b1d087dc4987793
dannypernik <dannypernik@mail.com>
2023-01-11 02:13:38 -0600

    rm unnecessary config vars

--- config.py
+++ config.py
@@ -18,9 +18,6 @@ class Config(object):
     HCAPTCHA_SECRET_KEY = os.environ.get('HCAPTCHA_SECRET_KEY')
     MAILJET_KEY = os.environ.get('MAILJET_KEY')
     MAILJET_SECRET = os.environ.get('MAILJET_SECRET')
-    MOM_EMAIL = os.environ.get('MOM_EMAIL')
-    DAD_EMAIL = os.environ.get('DAD_EMAIL')
     ADMINS = [os.environ.get('ADMINS')]
     HELLO_EMAIL = os.environ.get('HELLO_EMAIL')
     PHONE = os.environ.get('PHONE')
-    SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID')
